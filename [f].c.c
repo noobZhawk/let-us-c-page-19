b2b5802253d03389dc1a4c2b5b18943c6786c15b
Hrithik Barman <87934722+noobZhawk@users.noreply.github.com>
2023-11-04 02:57:26 +0600

    Update [f].c.c

--- [f].c.c
+++ [f].c.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    int w=1189;
+    int h=841;
+
+    printf("Paper Size A0: %d mm * %d mm\n",w,h);
+
+    int i;
+    for (i = 1; i <= 8; i++)
+    {
+
+        int n_w=h;
+        int n_h=w/2;
+
+        w= n_w;
+        h= n_h;
+
+        printf("Paper Size A%d: %d mm * %d mm\n", i, w, h);
+    }
+
+    return 0;
+}
 
