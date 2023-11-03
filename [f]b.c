efab9d7dd6ca66a783542c0aa6d1fe822e982a32
Hrithik Barman <87934722+noobZhawk@users.noreply.github.com>
2023-11-04 02:56:25 +0600

    Update [f]b.c

--- [f]b.c
+++ [f]b.c
@@ -6,12 +6,12 @@ int main()
     float r;
     printf("Enter the Length and breadth:");
     scanf("%d %d",&l,&b);
-    printf("Enter the radius of Circle:")
+    printf("Enter the radius of Circle:");
     scanf("%f",&r);
 
 
-    printf("area of rectangle :%d\n perimeter of rectangle :%d\n",l*b,);
-    printf("the are of circle")
+    printf(" area of rectangle :%d\n perimeter of rectangle :%d\n",l*b,2*(l+b));
+    printf(" area of circle :%.4f\n Circumference of circle :%.4f\n",pi*r*r,2*pi*r);
 
 
 }
