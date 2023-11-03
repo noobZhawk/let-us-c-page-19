02348bcdef219065138a9cb28b384767e1771766
Hrithik Barman <87934722+noobZhawk@users.noreply.github.com>
2023-11-04 02:41:09 +0600

    Add files via upload

---
+++ [f]a.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    float f;
+    printf("Temperature of a city in fahrenheit degrees:");
+    scanf("%f",&f);
+
+    float c;
+    c=(f-32)*5/9;
+
+    printf("temperature in centigrade degrees: %f",c);
+
+
+
+
+
+}
---
+++ [f]b.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define pi 3.14159
+int main()
+{
+    int l,b;
+    float r;
+    printf("Enter the Length and breadth:");
+    scanf("%d %d",&l,&b);
+    printf("Enter the radius of Circle:")
+    scanf("%f",&r);
+
+
+    printf("area of rectangle :%d\n perimeter of rectangle :%d\n",l*b,);
+    printf("the are of circle")
+
+
+}
